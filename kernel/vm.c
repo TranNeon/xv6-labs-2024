2b4825782d819ad9590ff41ce245b1b17914f97a
TrieuMan <JamesChung20112005@gmail.com>
2025-04-10 09:14:17 +0000

    defs exec vm.c changes

--- kernel/vm.c
+++ kernel/vm.c
@@ -488,10 +488,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
 
 
 #ifdef LAB_PGTBL
-void
-vmprint(pagetable_t pagetable) {
-  // your code here
+void vmprint_helper(pagetable_t pagetable, int depth) {
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      for (int d = 0; d < depth; d++)
+        printf(".. ");
+      
+      uint64 pa = PTE2PA(pte);
+      printf("..%d: pte %ld pa %ld\n", i, pte, pa);
+      
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) { // Non-leaf node (page directory)
+        vmprint_helper((pagetable_t)pa, depth + 1);
+      }
+    }
+  }
 }
+
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  vmprint_helper(pagetable, 1);
+}
+
 #endif
 
 
